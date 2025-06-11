da2a5e2c721fa145deb1b42039ea823287147eb3
ThorowZ <dennisgranatdennisgranat@hotmail.com>
2025-06-11 23:42:13 +0200

    Calendar structure

--- src/assets/pages/Calendar.jsx
+++ src/assets/pages/Calendar.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 export default function Calendar() {
 
-  const days = Array.from({ length: 35 }, (_, i) => i + 1);
+  const days = Array.from({ length: 35 }, (_, i) =>  i < 31 ? i + 1 : i - 30);
 
   if (days.length < 31) {
-    
+    const remainingDays = 31 - days.length;
+  
   }
   return (
     <div className='Calendar-container'>
